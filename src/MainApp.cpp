6477cf5e63d9205390aaf1bd6a44ae72356b2a9c
Kirill Katz <xnlyre@protonmail.com>
2025-01-01 15:35:02 +0200

    Add movable light

--- src/MainApp.cpp
+++ src/MainApp.cpp
@@ -19,8 +19,10 @@
 namespace Application {
 
 struct GlobalUbo {
-    alignas(16) glm::mat4 projectionView{1.f};
-    alignas(16) glm::vec3 lightDirection = glm::normalize(glm::vec3{1.f, -3.f, -1.f});
+    glm::mat4 projectionView{1.f};
+    glm::vec4 ambientLightColor{1.f, 1.f, 1.f, 0.02f}; // alpha is the intensity
+    glm::vec3 lightPosition{-1.f};
+    alignas(16) glm::vec4 lightColor{1.f}; // alpha is the intensity
 };
 
 MainApp::MainApp() {
@@ -51,7 +53,7 @@ void MainApp::run() {
 
     auto globalSetLayout =
         DescriptorSetLayout::Builder(device)
-            .addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT)
+            .addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL)
             .build();
 
     std::vector<VkDescriptorSet> globalDescriptorSets(SwapChain::MAX_FRAMES_IN_FLIGHT);
@@ -73,6 +75,8 @@ void MainApp::run() {
     camera.setViewTarget(glm::vec3(-1.f, -2.f, 2.f), glm::vec3(0.f, 0.f, 2.5f));
 
     auto viewerObject = SceneObject::createSceneObject();
+    viewerObject.transform.translation.z = -2.5;
+
     MovementController cameraController{};
 
     auto currentTime = std::chrono::high_resolution_clock::now();
@@ -89,7 +93,7 @@ void MainApp::run() {
 
         float aspect = renderer.getAspectRatio();
         // camera.setOrthographicProjection(-aspect, aspect, -1, 1, -1, 1);
-        camera.setPerspectiveProjection(glm::radians(90.f), aspect, 0.1f, 10.f);
+        camera.setPerspectiveProjection(glm::radians(90.f), aspect, 0.1f, 100.f);
 
         if (auto commandBuffer = renderer.beginFrame()) {
             int frameIndex = renderer.getFrameIndex();
@@ -122,18 +126,23 @@ void MainApp::loadSceneObjects() {
     std::shared_ptr<Model> model = Model::createModelFromFile(device, "models/flat_vase.obj");
     auto sceneObj = SceneObject::createSceneObject();
     sceneObj.model = model;
-    sceneObj.transform.translation = {.0f, .5f, 2.5f};
+    sceneObj.transform.translation = {.0f, .5f, 1.f};
     sceneObj.transform.scale = 10.f;
-
     sceneObjects.push_back(std::move(sceneObj));
 
     std::shared_ptr<Model> model2 = Model::createModelFromFile(device, "models/flat_vase.obj");
     auto sceneObj2 = SceneObject::createSceneObject();
-    sceneObj2.model = model;
-    sceneObj2.transform.translation = {2.f, .1f, 3.5f};
+    sceneObj2.model = model2;
+    sceneObj2.transform.translation = {2.f, .1f, 1.f};
     sceneObj2.transform.scale = 5.f;
-
     sceneObjects.push_back(std::move(sceneObj2));
+
+    std::shared_ptr<Model> floor = Model::createModelFromFile(device, "models/quad.obj");
+    auto floorObj = SceneObject::createSceneObject();
+    floorObj.model = floor;
+    floorObj.transform.translation = {0.f, -0.5f, 0.f};
+    floorObj.transform.scale = 3.f;
+    sceneObjects.push_back(std::move(floorObj));
 }
 
 }
